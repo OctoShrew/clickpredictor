a30a0d8e23e6ef61b7755a993997dd2887c0e4cf
Felix Quinque <felix.quinque@gmail.com>
2022-06-24 12:40:05 +0100

    Initial Commit: Adding draft for mock-api

---
+++ api.py
@@ -0,0 +1,37 @@
+import json
+from fastapi import FastAPI
+import numpy as np
+from typing import Dict, List
+app = FastAPI()
+
+
+# this should of course be a post method but doesn't work for browser testing
+@app.get("/log_click", status_code=201)
+def log_click(user_click: int, session_id: str) -> Dict:
+    """
+    user_click (int):  The click made by the user
+    session_id (str): The session_id 
+
+    logs a user's most recent click to store in the database and returns the user's percentage and             standard_deviation
+
+    returns {percentage: float, standard_deviation: float}
+    """
+    pass
+
+
+@app.get("/get_data")
+def get_data() -> List[float]:
+    """
+    returns a histogram of the performances achieved by users (based on the database) where each
+    element contains the number of people having achieved a certain percentage
+    """
+
+    dist = np.random.normal(loc=65, scale=10, size=10000)
+    dist = dist[dist < 100]
+    dist = dist[dist > 0]
+    bins = np.linspace(0, 100, 100)
+
+    histogram = np.zeros_like(bins)
+    bin_indexes = np.searchsorted(bins, dist)
+    np.add.at(histogram, bin_indexes, 1)
+    return list(histogram)
