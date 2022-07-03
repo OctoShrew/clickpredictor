cb06543a2bdb28a3a4c8adc2cd5bf764f21a9f29
Felix Quinque <felix.quinque@gmail.com>
2022-07-03 12:10:44 +0100

    fixed multithreading issue

--- api.py
+++ api.py
@@ -3,15 +3,16 @@ from fastapi import FastAPI
 import numpy as np
 from typing import Dict, List
 from db import create_connection,delete_db,check_session,update_session,insert_session
+import sqlite3
 app = FastAPI()
 database = r"clicks.db"
 
 # create a database connection
-conn = create_connection(database)
-
+# conn = create_connection(database, connect_args={'check_same_thread': False})
+conn = con = sqlite3.connect(database, check_same_thread = False)
 
 # this should of course be a post method but doesn't work for browser testing
-#@app.get("/log_click", status_code=201)
+@app.get("/log_click", status_code=201)
 def log_click(user_click: int, predicted_click: int, session_id: str) -> Dict:
     """
     user_click (int):  The click made by the user
@@ -29,7 +30,7 @@ def log_click(user_click: int, predicted_click: int, session_id: str) -> Dict:
         
 
 
-#@app.get("/get_data")
+@app.get("/get_data")
 def get_data() -> List[float]:
     """
     returns a histogram of the performances achieved by users (based on the database) where each
@@ -50,8 +51,8 @@ def get_data() -> List[float]:
     np.add.at(histogram, bin_indexes, 1)
     return list(histogram)
     
-log_click(1,1,"1")
-log_click(1,1,"1")
-log_click(2,1,"2")
-get_data()
+# log_click(1,1,"1")
+# log_click(1,1,"1")
+# log_click(2,1,"2")
+# get_data()
     
